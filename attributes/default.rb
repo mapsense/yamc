9dd55c7e149696261fcf87c9fdd48dbffe56f4de
Misha Soliterman <msoliter@gmail.com>
2014-10-21 14:10:02 -0700

    Updated recipe to work with integration smoke testing.

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,9 +11,10 @@ automatic['mongodb']['packages'] = [
 # The version of MongoDB to install.
 default['mongodb']['version'] = '2.6.1'
 
-# The MongoDB distribution to install. 
+# The MongoDB distribution to install. (These are just parameters to apt-get.) 
 default['mongodb']['distribution'] = {
-  'name'  => '10gen',
-  'uri'   => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart'
+  'name'       => 'dist',
+  'components' => [ '10gen' ],
+  'uri'        => 'http://downloads-distro.mongodb.org/repo/ubuntu-upstart'
 }
 
--- recipes/install_ubuntu.rb
+++ recipes/install_ubuntu.rb
@@ -1,11 +1,13 @@
 apt_repository 'mongodb' do
   uri           node['mongodb']['distribution']['uri']
   distribution  node['mongodb']['distribution']['name']
+  components    node['mongodb']['distribution']['components']
   keyserver     'hkp://keyserver.ubuntu.com:80'
   key           '7F0CEB10'
+  action        :add
 end
 
-include_recipe 'apt::default'
+include_recipe 'apt'
 
 node['mongodb']['packages'].map do |name|
   package name do
